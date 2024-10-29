ed49224bb50ea52f1a18ba27921f5b2648a225d4
Aman Choudhary <aman.choudhary7722@gmail.com>
2024-10-29 16:33:32 +0530

    adding blog & CFP page and pdf

---
+++ src/pages/CallForProposal.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { siteContent } from '../constants/content'; 
+import '../styles/main.css';
+
+const CallForProposals = () => {
+  const { cardDataForCFP } = siteContent;
+
+  const handleButtonClick = (button) => {
+    const { pdfLink, email, subject, body } = button;
+    if (pdfLink) {
+      window.open(pdfLink, '_blank');
+    } else if (email) {
+      const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject || '')}&body=${encodeURIComponent(body || '')}`;
+      window.location.href = mailtoLink;
+    } else {
+      console.log('Button clicked with no action.');
+    }
+  };
+
+  return (
+    <div className="card-list">
+      {cardDataForCFP.map((card) => (
+        <div key={card.id} className="cardArea">
+          <div className="card-content">
+            <h3 className="card-heading">{card.mainHeading}</h3>
+            {card.subHeading && <h4 className="card-subheading">{card.subHeading}</h4>}
+            <p className="card-description">{card.description}</p>
+            <p className="last-date">{card.lastDate}</p>
+          </div>
+          <div className="card-buttons-list">
+            {card.buttons.map((button, index) => (
+              <button
+                key={index}
+                className="card-buttons"
+                onClick={() => handleButtonClick(button)}
+              >
+                {button.text}
+              </button>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CallForProposals;
