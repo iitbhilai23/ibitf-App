6ca5138f7490f9bf457633195f82223dfa04e959
iitbhilai23 <psureshkumar@iitbhilai.ac.in>
2024-10-29 15:43:22 +0530

    blog page updated 28-10-24

--- src/pages/Schemes.js
+++ src/pages/Schemes.js
@@ -1,13 +1,13 @@
 
 import React from 'react';
-import { siteContent } from '../constants/content'; 
-import '../styles/main.css'; 
+import { siteContent } from '../constants/content';
+import '../styles/main.css';
 
 const Schemes = () => {
-  const { cardDetail } = siteContent; 
+  const { cardDetail } = siteContent;
 
   return (
-    <div className="card-stack">
+    <div className="card-stack1">
       {cardDetail.map((card, index) => (
         <div className="card" key={index}>
           <h3 className="card-title">{card.title}</h3>
