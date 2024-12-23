7413ad61f35c8041be027783b8e3b42571d41820
Sankalp Ramteke <sankalprt658@gmail.com>
2024-12-23 17:20:47 +0530

    Hackathon Popup Updated

--- src/components/popup/Popup.js
+++ src/components/popup/Popup.js
@@ -1,25 +1,68 @@
 
+// import React from "react";
+// import "./Popup.css"; 
+// import PopImage from '../../assets/popup/Hackathon-PMAY.jpeg';
+
+// const Popup = ({ show, closePopup }) => {
+//   if (!show) return null; 
+
+//   return (
+//     <div className="popup-overlay">
+//       <div className="popup-box">
+//         <button className="close-button" onClick={closePopup}>
+//           &times;
+//         </button>
+//         <img
+//           src={PopImage} 
+//           alt="Popup"
+//             className="popup-image"
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Popup;
+
+
 import React from "react";
-import "./Popup.css"; 
-import PopImage from '../../assets/popup/Hackathon_Banner24.jpg';
+import "./Popup.css";
+import PopImage from '../../assets/popup/Hackathon-PMAY.jpeg';
 
 const Popup = ({ show, closePopup }) => {
-  if (!show) return null; 
+  if (!show) return null;
+
+  const openHackathonLink = () => {
+    window.open("https://tihiitbhilai.accubate.app/ext/form/2982/1/apply"); // Using hackathon link from siteContent
+  };
+
+  const handleCloseClick = (event) => {
+    event.stopPropagation(); // Prevent the event from propagating to the popup-box
+    closePopup(); // Close the popup
+  };
 
   return (
     <div className="popup-overlay">
       <div className="popup-box">
-        <button className="close-button" onClick={closePopup}>
-          &times;
-        </button>
-        <img
-          src={PopImage} 
-          alt="Popup"
+        <div className="image-container">
+          <img
+            src={PopImage}
+            alt="Popup"
             className="popup-image"
-        />
+            onClick={openHackathonLink}  // Only trigger the link when clicking the image
+          />
+          <button className="close-button" onClick={handleCloseClick}>
+            &times;
+          </button>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Popup;
+
+
+
+
+
