0c9952871ca8f2cd8e5632d552761061a3d7b0f0
Sankalp Ramteke <sankalprt658@gmail.com>
2024-12-23 17:28:45 +0530

    popup

--- src/components/popup/Popup.js
+++ src/components/popup/Popup.js
@@ -1,28 +1,4 @@
 
-// import React from "react";
-// import "./Popup.css"; 
-// import PopImage from '../../assets/popup/Hackathon-PMAY.jpeg';
-
-// const Popup = ({ show, closePopup }) => {
-//   if (!show) return null; 
-
-//   return (
-//     <div className="popup-overlay">
-//       <div className="popup-box">
-//         <button className="close-button" onClick={closePopup}>
-//           &times;
-//         </button>
-//         <img
-//           src={PopImage} 
-//           alt="Popup"
-//             className="popup-image"
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Popup;
 
 
 import React from "react";
@@ -33,12 +9,12 @@ const Popup = ({ show, closePopup }) => {
   if (!show) return null;
 
   const openHackathonLink = () => {
-    window.open("https://tihiitbhilai.accubate.app/ext/form/2982/1/apply"); // Using hackathon link from siteContent
+    window.open("https://tihiitbhilai.accubate.app/ext/form/2982/1/apply"); 
   };
 
   const handleCloseClick = (event) => {
-    event.stopPropagation(); // Prevent the event from propagating to the popup-box
-    closePopup(); // Close the popup
+    event.stopPropagation(); 
+    closePopup(); 
   };
 
   return (
