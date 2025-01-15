5e28b2761174971a5483308090730b6860ff20ee
Sankalp Ramteke <sankalprt658@gmail.com>
2025-01-15 12:46:44 +0530

    Pop-Up Removed

--- src/components/popup/Popup.js
+++ src/components/popup/Popup.js
@@ -1,44 +1,38 @@
+// import React from "react";
+// import "./Popup.css";
+// import popup1 from '../../assets/popup/EXPERT_TALK.jpg';
+// import popup2 from "../../assets/popup/popup2.jpg";
 
 
+// const Popup = ({ show, closePopup }) => {
+//   if (!show) return null;
 
-import React from "react";
-import "./Popup.css";
-import PopImage from '../../assets/popup/EXPERT_TALK.jpg';
+//   const openHackathonLink = () => {
+//     window.open("https://tihiitbhilai.accubate.app/ext/form/2982/1/apply"); 
+//   };
+//   const handleCloseClick = (event) => {
+//     event.stopPropagation(); 
+//     closePopup(); 
+//   };
 
-const Popup = ({ show, closePopup }) => {
-  if (!show) return null;
-
-  const openHackathonLink = () => {
-    window.open("https://tihiitbhilai.accubate.app/ext/form/2982/1/apply"); 
-  };
-  const handleCloseClick = (event) => {
-    event.stopPropagation(); 
-    closePopup(); 
-  };
-
-  return (
-    <div className="popup-overlay">
-      <div className="popup-box">
-        <div className="image-container">
-          <img
-            src={PopImage}
-            alt="Popup"
-            className="popup-image"
-            onClick={openHackathonLink}  
-          />
-          <button className="close-button" onClick={handleCloseClick}>
-            &times;
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className="popup-overlay">
+//       <div className="popup-box">
+//         <div className="image-container">
+//           <img
+//             src={popup1}
+//             alt="Popup"
+//             className="popup-image"
+//             onClick={openHackathonLink}  
+//           />
+//           <button className="close-button" onClick={handleCloseClick}>
+//             &times;
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
 
-export default Popup;
-
-
-
-
-
+// export default Popup;
