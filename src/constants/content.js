bdfcfa70193151a733c3ed9ca198c2bd36fa739c
Suresh Suri <psureshkumar@iitbhilai.ac.in>
2025-05-16 10:57:48 +0530

    update vsk jobs status

--- src/constants/content.js
+++ src/constants/content.js
@@ -1230,7 +1230,8 @@ export const siteContent = {
       route: "/videoKYC",
     },
   ],
-  
+
+
   videoPage: {
     title: "The Hon'ble President of India Applauds Our Innovation",
     description:
@@ -1248,7 +1249,7 @@ export const siteContent = {
       contractPeriod:"1 Year",
       ageLimit: "Maximum Age 45 years",
       location: "IBITF IIT Bhilai",
-      lastDate: "15th April 2025",   
+      lastDate: "17th April 2025",   
       downloadLink: require("../assets/PDF/career/VSK2025V2.pdf"),
       applyLink: "https://docs.google.com/forms/d/e/1FAIpQLSeeDs2spmQb25x8c6nbTukDqSGQAKLDQsYWaGby1ePTFxrjcw/viewform",
       applyText: "Apply Now",
