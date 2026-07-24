4654a607d73dc952ab33a0afd6f9fd3062dc651f
Sankalp Ramteke <sankalprt658@gmail.com>
2026-07-24 12:02:46 +0530

    feat: add content constants and asset configurations for IBITF project management

--- src/constants/content.js
+++ src/constants/content.js
@@ -1389,7 +1389,7 @@ export const siteContent = {
       location: "IBITF, IIT Bhilai",
       lastDate: "27 July 2026",
       downloadLink: require("../assets/PDF/career/_AdvertisementforFinteQproject.pdf"),
-      applyLink: "#",
+      applyLink: "https://docs.google.com/forms/d/e/1FAIpQLSd90EBzgvqOHKMneaG5Hv_1_g5Z2D7yiyOGTR9dtPgB-OwIGQ/viewform",
       applyText: "Apply Now",
     },
     {
